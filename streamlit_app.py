462938e2c2d0fe6b1bcbddd78be242871bbf5ff7
ajinkyasnowflake <137598766+ajinkyasnowflake@users.noreply.github.com>
2023-06-24 12:21:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,9 +2,12 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
 
-streamlit.header('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 Kale , Spinach & Rocket Smoothie')
 streamlit.text('🐔 Breakfast Menu')
 streamlit.text('🥑🍞 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 
   
